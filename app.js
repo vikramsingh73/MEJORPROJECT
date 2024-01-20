b6346de24e58cec0cd2c4343cd7bcc67c4c139c2
vikram singh <vikramsing73750@gmail.com>
2024-01-20 15:52:56 +0530

    change

--- app.js
+++ app.js
@@ -90,9 +90,9 @@ app.use((req,res,next)=>{
   next();
 });
 
-app.get("/", (req,res)=>{
-  res.render("./listings/home.ejs")
-});
+app.get("/",(req,res)=>{
+     res.render("listings/show.ejs",{allListings});
+    });
 
 
 
